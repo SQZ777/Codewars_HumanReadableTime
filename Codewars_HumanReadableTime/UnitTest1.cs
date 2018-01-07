3fa329d8e2e32761323ce362a29c0de2b3623694
sqz777 <sqz777isme@gmail.com>
2018-01-07 19:59:48 +0800

    加入專案檔案。

---
+++ Codewars_HumanReadableTime/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codewars_HumanReadableTime
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
