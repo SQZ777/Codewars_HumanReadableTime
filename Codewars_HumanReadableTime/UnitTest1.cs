c9fb74bd469c2e988429d92e0cde29b9ea315101
sqz777 <sqz777isme@gmail.com>
2018-01-07 20:55:38 +0800

    fail GetMin test case : input 0

--- Codewars_HumanReadableTime/UnitTest1.cs
+++ Codewars_HumanReadableTime/UnitTest1.cs
@@ -14,7 +14,13 @@ namespace Codewars_HumanReadableTime
         [TestMethod]
         public void GetHour_Input_3600_Should_Be_1()
         {
-            Assert.AreEqual(1,TimeFormat.GetHour(3600));
+            Assert.AreEqual(1, TimeFormat.GetHour(3600));
+        }
+
+        [TestMethod]
+        public void GetMin_Input_0_Should_Be_0()
+        {
+            Assert.AreEqual(0,TimeFormat.GetMin(0));
         }
     }
 
@@ -22,7 +28,12 @@ namespace Codewars_HumanReadableTime
     {
         public static int GetHour(int num)
         {
-            return 0;
+            return num / 3600;
+        }
+
+        public static int GetMin(int num)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
